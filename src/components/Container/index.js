cff1d88594c088c6db92def60a6cfa006fd35deb
medhinioak <medhinioak@gmail.com>
2020-02-13 11:26:23 +0530

    added api call

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Button from '../Button';
-import TextBox from '../../TextBox';
+import TextBox from '../TextBox';
+import axios from 'axios'
+import url from '../../constants'
 
 class Container extends Component {
     state={
         text:''
+    }
+    componentDidMount = async() => {
+        const response = await axios.get(url);
+        this.setState({
+            text: response.data.initialText
+        })
+
     }
 
     onChange=(inputTextValue)=>{
@@ -17,7 +26,7 @@ class Container extends Component {
         const {testId,testIdButton,testIdTextBox}=this.props;
         return (
             <div data-testid={testId}>
-                <TextBox onChange={(this.onChange)} testId={testIdTextBox} />
+                <TextBox onChange={(this.onChange)} testId={testIdTextBox} value = {this.state.text}/>
                 <Button text={this.state.text} buttonType="round" testId={testIdButton}/>
             </div>
         )
--- src/components/Container/index.test.js
+++ src/components/Container/index.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, wait } from '@testing-library/react';
 import Container from '../Container';
+import axios from 'axios';
+import url from '../../constants'
 
 describe('The container component',()=>{
     it('should check if the button component works',()=>{
@@ -16,5 +18,13 @@ describe('The container component',()=>{
 
        expect(getByTestId('test-btn')).toHaveTextContent('Bhumika clicked 0 times.');
 
+    })
+    it('Should display the content from the api', async() => {
+        const mockAxios = jest.spyOn(axios, 'get');
+        mockAxios.mockResolvedValue({data:{initialText:'unicorn'}});
+
+        const {getByTestId}= await render(<Container testId='test-cntner' testIdButton='test-btn' testIdTextBox='123'/>)
+        expect(mockAxios).toHaveBeenCalledWith(url)
+        await wait(() => expect(getByTestId('123').value).toBe('unicorn'));
     })
 })
